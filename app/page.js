2e813c8ea7359dc5253b21aa7a2bf464fdfe8634
kanchan sharma <kanusharma881388@gmail.com>
2025-11-11 11:59:49 +0530

    Fix: Corrected typo in section1 component import

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export const metadata = {
 
 
 import Hero from "@/components/Hero";
-import Section1 from "@/components/section1.jsx";
+import Section1 from "@/components/Section1";
 import Section2 from "@/components/Section2";
 import Section3 from "@/components/Section3";
 import Section4 from "@/components/Section4";
